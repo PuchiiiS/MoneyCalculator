0df34dc5ff49f3edd43742b5998c28c1d54ed540
Estados <hsuliman@gmail.com>
2024-12-26 22:46:48 +0000

    SwingMain Updated(Solo falta comprobar los decimales)

--- src/main/java/software/ulpgc/moneycalculator/swing/SwingMain.java
+++ src/main/java/software/ulpgc/moneycalculator/swing/SwingMain.java
@@ -2,6 +2,7 @@ package software.ulpgc.moneycalculator.swing;
 
 import software.ulpgc.moneycalculator.*;
 import software.ulpgc.moneycalculator.fixerws.FixerCurrencyLoader;
+import software.ulpgc.moneycalculator.fixerws.FixerExchangeRateLoader;
 import software.ulpgc.moneycalculator.mocks.MockExchangeRateLoader;
 
 
@@ -12,40 +13,45 @@ import java.util.List;
 import java.util.Map;
 
 public class SwingMain extends JFrame {
-    private final Map<String,Command> commands = new HashMap<>();
+    private final Map<String, Command> commands = new HashMap<>();
     private MoneyDisplay moneyDisplay;
     private MoneyDialog moneyDialog;
     private CurrencyDialog currencyDialog;
 
-    public static void main(String[] args) {
-        SwingMain main = new SwingMain();
-        List<Currency> currencies = new FixerCurrencyLoader().load();
-        Command command = new ExchangeMoneyCommand(
-                main.moneyDialog().define(currencies),
-                main.currencyDialog().define(currencies),
-                new MockExchangeRateLoader(),
-                main.moneyDisplay());
-        main.add("exchange money", command);
-        main.setVisible(true);
-    }
-
     public SwingMain() throws HeadlessException {
-        this.setTitle("Money calculator");
-        this.setSize(800,600);
-        this.setLocationRelativeTo(null);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new FlowLayout());
+        setTitle("MoneyCalculator");
+        setSize(800, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
         this.add(createMoneyDialog());
         this.add(createCurrencyDialog());
         this.add(createMoneyDisplay());
         this.add(toolbar());
     }
 
+    public static void main(String[] args) {
+
+        SwingMain main = new SwingMain();
+        List<Currency> currencies = new FixerCurrencyLoader().load();
+        Command command = new ExchangeMoneyCommand(
+                main.moneyDialog().define(currencies),
+                main.currencyDialog().define(currencies),
+                new FixerExchangeRateLoader(),
+                main.moneyDisplay());
+        main.add("exchange money", command);
+        main.setVisible(true);
+    }
+
     private Component toolbar() {
         JButton button = new JButton("calculate");
+        button.setFont(new Font("Serif", Font.BOLD, 16));
+        button.setForeground(Color.BLUE);
+        button.setToolTipText("This is an example button");
         button.addActionListener(e -> commands.get("exchange money").execute());
         return button;
     }
+
+
 
     private Component createMoneyDisplay() {
         SwingMoneyDisplay display = new SwingMoneyDisplay();
